48f8f312fc4ce4021a29f7bf3449777beb2b1a13
chaeyeonan <dkscodus1204@ewhain.net>
2024-01-16 10:59:10 +0900

    vue webpack 설치 및 초기 세팅

---
+++ todolist/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+require("@babel/polyfill");
+
+module.exports = {
+  //진입점
+  entry: {
+    app: ["@babel/polyfill", path.join(__dirname, "main.js")], //dirname : 현재경로
+  },
+  //결과물에 대한 설정
+  output: {
+    filename: "[name].js", //app.js
+    path: path.join(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      // ... other rules
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    // make sure to include the plugin!
+    new VueLoaderPlugin(),
+  ],
+};
