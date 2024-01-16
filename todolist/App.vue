8b98a22463a23ea866b9648b8061a7bb7646b87f
chaeyeonan <dkscodus1204@ewhain.net>
2024-01-16 10:58:43 +0900

    vue webpack 초기 세팅

---
+++ todolist/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello vue",
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
